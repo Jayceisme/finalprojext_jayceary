ef90dae2d901440d75f30f1c3bceeb0e6389d849
Jayceisme <111900048+Jayceisme@users.noreply.github.com>
2022-11-07 12:07:54 -0500

    final project

---
+++ final project/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int room = 1; //This keeps track of what room the player is in. 
+        string response = ""; //This keeps track of what the player wants to do
+        bool gameFinished = false;
+        bool hasKey = false;
+
+        //While they are still playing let them type in new responses
+        while (gameFinished == false)
+        {
+            Console.WriteLine("You go to the local gas station, do you get coke or pepsi?");
+            response = Console.ReadLine();
+
+            if (response.Equals("coke"))
+            {
+                Console.WriteLine("You walk up to the restiture and the cashier says 2 dollars please. Do You give him your moeney?");
+                response = Console.ReadLine();
+                {
+                    if (response.Equals("yes"))
+                    {
+                        Console.WriteLine("You made it out of the gas station and back home.");
+                        {
+                            gameFinished = true;
+                        }
+                        
+                    }
+                    if (response.Equals("no"))
+                    {
+                        Console.WriteLine("You end up in the back of a police car.");
+                        {
+                            gameFinished = true;
+                        }
+
+                    }
+                }
+            }
+            if (response.Equals("pepsi"))
+            {
+                Console.WriteLine("You walk up to the restiture and the cashier gives you a discusted look and says 2 dollars please. Do You give him your moeney?");
+            }
+            {
+                if (response.Equals("yes"))
+                {
+                    Console.WriteLine("You made it out of the gas station and back home.");
+                    response = Console.ReadLine();
+                    {
+                        gameFinished = true;
+                    }
+
+                }
+                if (response.Equals("no"))
+                {
+                    Console.WriteLine("You end up in the back of a police car.");
+                    {
+                        gameFinished = true;
+                    }
+
+                }
+            }
+
+        }
+    }
+}
+
